ff642d93789ab79f5eef370b60566db5478cf63e
JMundondo <jephiastmundondo@gmail.com>
2024-10-09 17:13:02 +0200

    first commit

---
+++ src/functions/bgImage.js
@@ -0,0 +1,8 @@
+import localImage1 from "../logo/local-image-1.jpeg";
+import localImage2 from "../logo/local-image-2.jpeg";
+import localImage3 from "../logo/local-image-3.jpeg";
+import localImage4 from "../logo/local-image-4.jpeg";
+
+const image = [localImage1, localImage2, localImage3, localImage4];
+
+export default image;
